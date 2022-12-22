e0844139d8ade6c7cd7ac739ce9954fb2cb8e38d
sreekar-34 <95081023+sreekar-34@users.noreply.github.com>
2022-12-22 14:49:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+//https://teachablemachine.withgoogle.com/models/8piyTYheL/
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    image_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+   document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log("ml5version",ml5.version);
+
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/8piyTYheL/model.json",modelLoaded);
+
+function modelLoaded(){
+    console.log("model is loaded");
+}
